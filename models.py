d2a20cfde219035b60c18825b05a18ab75c99950
Jefferson Fontana <jeff.r2@gmail.com>
2023-03-13 01:00:31 -0300

    updating structure

--- models.py
+++ models.py
@@ -17,3 +17,9 @@ class Jogo(db.Model):
     categoria = db.relationship('Categoria', backref=db.backref('jogos', lazy=True))
     descricao = db.Column(db.Text, nullable=False)
     imagem = db.Column(db.String(100), nullable=True)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
